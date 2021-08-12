a666e4dca448a33fccfd8101dbb1fd6d62ebad87
Sudarshan Bhide <sudarshan.bhide@embold.io>
2021-08-12 09:37:59 +0530

    debug work dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
 echo $GITHUB_WORKSPACE
-ls $GITHUB_WORKSPACE
+var=$(ls $GITHUB_WORKSPACE)
+echo $var
 /embold/scanner/bin/embold-scanner -u ${1} -t ${2} -c ${3} -r ${4}  -sh /embold/corona
