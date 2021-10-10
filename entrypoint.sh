32844cbc3a63d6a7ca2605877317a6b159932e26
Sudarshan Bhide <sudarshan.bhide@acellere.com>
2021-10-10 18:18:13 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -l
 
 if [[ -n ${5} ]]; then
+  echo "using verbose mode"
   /embold/scanner/bin/embold-scanner -u ${1} -t ${2} -c ${3} -r ${4}  -v -sh /embold/corona
 else
+  echo "NOT using verbose mode"
   /embold/scanner/bin/embold-scanner -u ${1} -t ${2} -c ${3} -r ${4} -sh /embold/corona
 fi
